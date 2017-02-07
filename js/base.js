75c7a3fbb72ec23b84ed8a8a18bff6b3db3cde1e
Tasha (Mayes) Dular <telizabethm@gmail.com>
2017-02-07 10:15:53 -0500

    animation
    adding animation

---
+++ js/base.js
@@ -0,0 +1,20 @@
+//JavaScript Animation
+
+$(document).ready(function() {
+        $(window).scroll( function(){
+            $('.animated').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
+    
+});
